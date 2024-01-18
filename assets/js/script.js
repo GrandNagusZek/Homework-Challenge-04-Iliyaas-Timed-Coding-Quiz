079a13ed9fd82048e5b48c4f084a30def35fa1b0
GrandNagusZek <iliyas.mallard@yahoo.com>
2024-01-17 21:02:00 -0500

    fixed bug with start quiz button problem was found in html button id typo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,15 @@
 var introEl= document.getElementById("intro")
 var questionSectionEl= document.getElementById("question-section")
 var initialInputEl= document.getElementById("initial-input")
-var highscoreEl= customElement.getElementById("highscore")
+var highscoreEl= document.getElementById("highscore")
 var startQuizEL= document.getElementById("start-quiz")
 var questionTitleEl= document.getElementById("question-title")
 var choiceListEl= document.getElementById("choice-list")
 
 function startQuiz(){
-
+    alert("test")
+    introEl.classList.add("hide")
 
 }
 
- startQuizEL.addEventListener("click", startQuiz)
+ startQuizEL.addEventListener("click",startQuiz)
