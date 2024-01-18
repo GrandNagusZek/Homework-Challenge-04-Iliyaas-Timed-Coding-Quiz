26ca09d742d68cf22d0d06da94d2ecf800bd921f
GrandNagusZek <iliyas.mallard@yahoo.com>
2024-01-17 20:47:01 -0500

    created startquiz function to JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,13 @@ var introEl= document.getElementById("intro")
 var questionSectionEl= document.getElementById("question-section")
 var initialInputEl= document.getElementById("initial-input")
 var highscoreEl= customElement.getElementById("highscore")
-var startQuiz=El document.getElementById("start-quiz")
+var startQuizEL= document.getElementById("start-quiz")
 var questionTitleEl= document.getElementById("question-title")
-var  choiceListEl= document.getElementById("choice-list")
+var choiceListEl= document.getElementById("choice-list")
+
+function startQuiz(){
 
 
+}
+
+ startQuizEL.addEventListener("click", startQuiz)
