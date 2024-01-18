a69c061798d80eaa01b64c94733d66c16ee56a76
GrandNagusZek <iliyas.mallard@yahoo.com>
2024-01-18 00:31:43 -0500

    added var for the quiz interval timer and started working on the startTimer function

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,11 +5,21 @@ var highscoreEl= document.getElementById("highscore")
 var startQuizEL= document.getElementById("start-quiz")
 var questionTitleEl= document.getElementById("question-title")
 var choiceListEl= document.getElementById("choice-list")
+var setIntervalId
+var timeRemaining=questionData.length
 
 function startQuiz(){
     
+
     introEl.classList.add("hide")
+
+
     questionSectionEl.classList.remove("hide")
+
+    setIntervalId=setInterval(startTimer , 1000 )
+}
+
+function startTimer(){
 
 }
 
