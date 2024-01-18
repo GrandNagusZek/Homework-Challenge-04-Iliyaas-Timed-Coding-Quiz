a0b5be5cd9681f070595286cfa41978d044b3a1e
GrandNagusZek <iliyas.mallard@yahoo.com>
2024-01-17 20:39:54 -0500

    added element sectors to JS file

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+var introEl= document.getElementById("intro")
+var questionSectionEl= document.getElementById("question-section")
+var initialInputEl= document.getElementById("initial-input")
+var highscoreEl= customElement.getElementById("highscore")
+var startQuiz=El document.getElementById("start-quiz")
+var questionTitleEl= document.getElementById("question-title")
+var  choiceListEl= document.getElementById("choice-list")
+
+
