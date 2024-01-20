be619b5aa859f37c72bfd9da4080a09b6613f135
GrandNagusZek <iliyas.mallard@yahoo.com>
2024-01-20 14:44:45 -0500

    modified startTimer function trying to adjust how time is displayed

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ var highscoreEl= document.getElementById("highscore")
 var startQuizEL= document.getElementById("start-quiz")
 var questionTitleEl= document.getElementById("question-title")
 var choiceListEl= document.getElementById("choice-list")
-var timerEl= document.getElementById("")
+var timerEl= document.getElementById("timer")
 var setIntervalId
 var timeRemaining=questionData.length * 15
 
