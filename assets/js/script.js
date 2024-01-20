0e74718253cac8b0cd76e856d49508cd255210d3
GrandNagusZek <iliyas.mallard@yahoo.com>
2024-01-19 22:23:52 -0500

    modified startTimer function in JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,9 @@ var highscoreEl= document.getElementById("highscore")
 var startQuizEL= document.getElementById("start-quiz")
 var questionTitleEl= document.getElementById("question-title")
 var choiceListEl= document.getElementById("choice-list")
+var timerEl= document.getElementById("")
 var setIntervalId
-var timeRemaining=questionData.length
+var timeRemaining=questionData.length * 15
 
 function startQuiz(){
     
@@ -20,7 +21,7 @@ function startQuiz(){
 }
 
 function startTimer(){
-
+    timerEl.textContent=timeRemaining--
 }
 
  startQuizEL.addEventListener("click",startQuiz)
